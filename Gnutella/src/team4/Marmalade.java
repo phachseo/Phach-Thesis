ebc6e7d44fd0cf6ec5ff8e3f5983399f4be89349
kaka <phachseo@gmail.com>
2011-10-22 23:12:52 +0700

    sua systemout

--- Gnutella/src/team4/Marmalade.java
+++ Gnutella/src/team4/Marmalade.java
@@ -6,7 +6,7 @@ public class Marmalade
 {
     public static void main(String[] args)
     {
-        
+        System.out.println("asfjhjksdf");
 
 	System.out.println("Setting up hash tables...");
 	QHandler.initQueryTable();
