adc983123139ffb6faa4da4932749ea3df1df6c3
phachseo <phachseo@gmail.com>
2011-10-28 21:28:32 -0700

    e

---
+++ Gnutella/build/classes/Client/Searcher.rs
@@ -0,0 +1,16 @@
+Client.SearchPanel$DownloadAction
+Client.SearchPanel$ClearAction
+Client.SearchPanel
+Client.TabbedPaneFrame
+Client.ConnectionPanel$DeleteAction
+Client.ConnectionPanel$ConnectAction
+Client.MonitorPanel
+Client.Searcher
+Client.ConnectionPanel
+Client.SearchPanel$SearchAction
+Client.SearchPanel$OpenAction
+Client.SearchPanel$MRAction
+Client.TabbedPaneFrame$1
+Client.TabbedPaneFrame$MenuHandler
+Client.ConnectionPanel$DeleteConnectionAction
+Client.SearchPanel$FormAction
